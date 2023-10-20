95ec6f67c9c0c9ac67dd065290158808f6c5cea0
murtazatanveer <murtaza.tanveer488@gmail.com>
2023-10-20 06:04:59 +0500

    3rd

---
+++ programs/password.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main(){
+
+int pass=987654321;
+
+int g;
+
+while(1){
+printf("\nENTER PASSWORD : ");
+scanf("%d",&g);
+
+if(g==pass){
+    printf("\n\nCORRECT PASSOWRD\n\n");
+    break;
+}
+
+else{
+   printf("\nn\nINCORRECT PASSWORD TRY AGAIN\n");
+}
+
+
+}
+
+}
