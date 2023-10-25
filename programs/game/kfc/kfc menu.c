0ed1b6f88cc9b72df9a9698895dd65eadcdb0d74
murtazatanveer <murtaza.tanveer488@gmail.com>
2023-10-25 05:01:36 +0500

    pending kfc menu

---
+++ programs/game/kfc/kfc menu.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main(){
+printf("Designed By : Murtaza Tanveer\n");
+
+printf("\n\n_______WELCOME TO KFC_______");
+
+
+printf("\n\n\n1. To view Cold Drinks.");
+printf("\n\n2. To view Fries Items.");
+printf("\n\n3. To view Fast Food items.");
+printf("\n\n4. To view Chicken related items.");
+
+int choice;
+printf("\n\nEnter Choice : ");
+scanf("%d",&choice);
+
+if(choice==1){
+
+
+
+}
+
+}
